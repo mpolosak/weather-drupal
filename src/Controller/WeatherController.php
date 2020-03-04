28679b3512b2e3b9ae82253172aa6536682ad0ac
mpolosak <42770518+mpolosak@users.noreply.github.com>
2020-03-04 13:40:58 +0100

    Initial commit

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Drupal\weather\Controller;
+
+class WeatherController{
+  public function content()
+  {
+    $client = \Drupal::httpClient();
+    try{
+      // place your appid in place of {appid}
+      // TODO: set appid in settings
+      $response=$client->request('GET','api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&units=metric&lang=pl&appid={appid}');
+      $body = $response->getBody()->getContents();
+      $data = json_decode($body);
+    }
+    catch(auto $e)
+    {
+      // TODO: write what to do when exeption happens
+    }
+    return array(
+      '#theme' => 'weather',
+      '#weather'=>$data
+    );
+  }
+}
