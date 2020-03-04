e53122fc21463ed0f2a9ca17a8ce1c556fcbef78
mpolosak <42770518+mpolosak@users.noreply.github.com>
2020-03-04 14:20:48 +0100

    Get current weather description in selected language

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -5,10 +5,11 @@ class WeatherController{
   public function content()
   {
     $client = \Drupal::httpClient();
+    $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
     try{
       // place your appid in place of {appid}
       // TODO: set appid in settings
-      $response=$client->request('GET','api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&units=metric&lang=pl&appid={appid}');
+      $response=$client->request('GET','api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&units=metric&lang='.$language.'&appid={appid}');
       $body = $response->getBody()->getContents();
       $data = json_decode($body);
     }
