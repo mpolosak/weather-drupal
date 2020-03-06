87a5d6b62bfa1047cf48cacea1764021c0e2e591
mpolosak <42770518+mpolosak@users.noreply.github.com>
2020-03-06 08:34:49 +0100

    Init work on Weather block

---
+++ src/Plugin/Block/WeatherBlock.php
@@ -0,0 +1,37 @@
+<?php
+namespace Drupal\weather\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'weather' block.
+ *
+ * @Block(
+ *   id = "weather_block",
+ *   admin_label = @Translation("Weather block"),
+ * )
+ */
+class FaxBlock extends BlockBase {
+  /**
+  * {@inheritdoc}
+  */
+  public function build() {
+    $client = \Drupal::httpClient();
+    $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    try{
+      // place your appid in place of {appid}
+      // TODO: set appid in settings
+      $response=$client->request('GET','api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&units=metric&lang='.$language.'&appid={appid}');
+      $body = $response->getBody()->getContents();
+      $data = json_decode($body);
+    }
+    catch(auto $e)
+    {
+      // TODO: write what to do when exeption happens
+    }
+    return array(
+      '#theme' => 'weather',
+      '#weather'=>$data
+    );
+  }
+}
